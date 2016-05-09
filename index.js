1095d747ad29fd6811353df0dcbce4333b25260b
Philip A. Nunoo <ketay99@gmail.com>
2016-05-09 06:16:32 +0000

    output with third parameter

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 "user strict";
 const heavyDeciCount = require("./lib");
-
-var x = 9872,
-    y = 9884;
-
-// console.log(heavyDeciCount(x, y));
 const imageToAscii = require("image-to-ascii");
 
 // The path can be either a local path or an url
 imageToAscii("./git_github.png", function (err, converted) {
 
-    console.log(err || converted);
+  // console.log(err || converted);
 });
+
+var x = 9872,
+    y = 9884;
+
+// console.log(heavyDeciCount(x, y));)
+
+var results = heavyDeciCount(x, y, true);
+console.log("Number\t\tAverage\t\tIs Heavy");
+for (var i = 0; i < results.array.length; i++) {
+  var obj = results.array[i];
+  console.log(obj.number + "\t\t" + obj.average + "\t\t" + (obj.isHeavy? "Yes": "No"));
+}
+console.log("\nTotal Heavy Count:\t" + results.heavy);
+// console.log(results);
--- lib.js
+++ lib.js
@@ -19,7 +19,7 @@ var averageOfDigits = function(x){
   return sum/array.length;
 }
 
-var heavyDeciCount = function (x, y){
+var heavyDeciCount = function (x, y, output){
   if (x>y)
     throw new Error('X is greater than y')
 
@@ -29,14 +29,25 @@ var heavyDeciCount = function (x, y){
   if (y<=0)
     throw new Error('Y is invalid')
 
-  var count = 0;
+  var count = 0,
+      outputObject = {array: []};
 
   for (var i = x; i <= y; i++) {
-    if(averageOfDigits(i) > 7){
+    var avg = averageOfDigits(i);
+    var currentObject = {number: i, average: avg, isHeavy: false};
+    if(avg > 7){
       count ++;
+      currentObject.isHeavy = true;
     }
+    outputObject.array.push(currentObject);
   }
+  outputObject.x = x;
+  outputObject.y = y;
+  outputObject.count = outputObject.array.length;
+  outputObject.heavy = count;
 
+  if (output)
+    return outputObject
   return count;
 }
 
